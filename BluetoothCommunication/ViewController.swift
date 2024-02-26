3d07d48511996e6f92cc7d50d296bf1d9e70bee2
Taehyen Kang <appletaehyen@gmail.com>
2024-02-26 15:25:55 +0900

    Initial Commit

---
+++ BluetoothCommunication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BluetoothCommunication
+//
+//  Created by 3i-A1-2022-033 on 2/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
